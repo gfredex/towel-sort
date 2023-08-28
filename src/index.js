8584a6c06aeb82856c0b64c28303a34595655258
Dima Zemnitski <g.fredex@gmail.com>
2023-08-28 17:15:00 +0300

    fix: fixed solution for task

--- src/index.js
+++ src/index.js
@@ -4,19 +4,23 @@
 module.exports = function towelSort(matrix) {
   let sortArr = [];
   let result = [];
-  for (i = 0; i < matrix.length; i++) {
-    if (!(i % 2)) {
-      sortArr.push(matrix[i]);
-    } else {
-      sortArr.push(matrix[i].reverse());
+  if (matrix) {
+    for (i = 0; i < matrix.length; i++) {
+      if (!(i % 2)) {
+        sortArr.push(matrix[i]);
+      } else {
+        sortArr.push(matrix[i].reverse());
+      }
+    }
+
+    for (i = 0; i < sortArr.length; i++) {
+      for (j = 0; j < sortArr[i].length; j++) {
+        result.push(sortArr[i][j]);
+      }
     }
   }
 
-  for (i = 0; i < sortArr.length; i++) {
-    for (j = 0; j < sortArr[i].length; j++) {
-      result.push(sortArr[i][j]);
-    }
-  }
+
 
   return result;
 }
